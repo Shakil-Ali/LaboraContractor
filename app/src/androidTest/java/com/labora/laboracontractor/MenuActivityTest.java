297754460a42cdf9b1cdb86810f69edf23a98cc4
Tanveer Hossain <thoss003@gold.ac.uk>
2019-02-25 12:38:02 +0000

    Menu Activity Test started

---
+++ app/src/androidTest/java/com/labora/laboracontractor/MenuActivityTest.java
@@ -0,0 +1,16 @@
+package com.labora.laboracontractor;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MenuActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onClick() {
+    }
+}
